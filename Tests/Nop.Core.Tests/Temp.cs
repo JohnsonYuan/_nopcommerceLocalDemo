681baba2c0e4fc231edd87b8069e65cd881e71a4
lxYuan <285575495@qq.com>
2016-10-17 22:47:59 +0800

    Update nop.core

---
+++ Tests/Nop.Core.Tests/Temp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Tests
+{
+    class BaseEntity1 : BaseEntity
+    {
+    }
+    class BaseEntity2 : BaseEntity
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseEntity1 be1 = new BaseEntity1();
+            BaseEntity1 be11 = new BaseEntity1();
+            BaseEntity2 be2 = new BaseEntity2();
+            Dictionary<BaseEntity1, int> dic = new Dictionary<BaseEntity1, int>();
+            dic.Add(be1, 1);
+            dic.Add(be11, 2);
+        }
+    }
+}
