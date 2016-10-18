8f7c5618365abae4a599c0f2c6f509141513035f
lxYuan <285575495@qq.com>
2016-10-18 21:24:02 +0800

    update test

--- Tests/Nop.Core.Tests/Temp.cs
+++ Tests/Nop.Core.Tests/Temp.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nop.Core.Tests
-{
+{// Create a class having six properties.
+    public class PropertyClass
+    {
+        public String Property1
+        {
+            get { return "hello"; }
+        }
+        public static String PropertyStatic
+        {
+            get { return "HelloI"; }
+        }
+        public String Property2
+        {
+            get { return "hello"; }
+        }
+
+        protected String Property3
+        {
+            get { return "hello"; }
+        }
+
+        private Int32 Property4
+        {
+            get { return 32; }
+        }
+
+        internal String Property5
+        {
+            get { return "value"; }
+        }
+
+        protected internal String Property6
+        {
+            get { return "value"; }
+        }
+    }
+
     class BaseEntity1 : BaseEntity
     {
     }
@@ -17,6 +54,12 @@ namespace Nop.Core.Tests
     {
         static void Main(string[] args)
         {
+            Type t = typeof(PropertyClass);
+            // Get the public properties.
+            PropertyInfo[] propInfos = t.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            Console.WriteLine(propInfos.Length);
+            return;
+
             BaseEntity1 be1 = new BaseEntity1();
             BaseEntity1 be11 = new BaseEntity1();
             BaseEntity2 be2 = new BaseEntity2();
