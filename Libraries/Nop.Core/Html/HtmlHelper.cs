7ca4a977079d3a0425999c2dc182a87a94a84122
JohnsonYuan <ylxin1993@gmail.com>
2016-10-26 17:32:34 +0800

    Continue Libraries/Nop.Core/Html

---
+++ Libraries/Nop.Core/Html/HtmlHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Html
+{
+    /// <summary>
+    /// Represents a HTML helper
+    /// </summary>
+    public partial class HtmlHelper
+    {
+        #region Fields
+
+        private readonly static Regex paragraphStartRegex = new Regex("<p>", RegexOptions.IgnoreCase);
+        private readonly static Regex paragraphEndRegex = new Regex("</p>", RegexOptions.IgnoreCase);
+        //private static Regex ampRegex = new Regex("&(?!(?:#[0-9]{2,4};|[a-z0-9]+;))", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        #endregion
+
+        #region Utilities
+
+        private static string EnsureOnlyAllowedHtml(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return string.Empty;
+
+            const string allowedTags = "br,hr,b,i,u,a,div,ol,ul,li,blockquote,img,span,p,em,strong,font,pre,h1,h2,h3,h4,h5,h6,address,cite";
+        }
+
+        private static bool IsValidTag(string tag, string tags)
+        {
+            string[] allowedTags = tags.Split(',');
+            if (tag.IndexOf("javascript") >= 0) return false;
+            if (tag.IndexOf("vbscript") >= 0) return false;
+            if (tag.IndexOf("onclick") >= 0) return false;
+
+            var endchars = new[] { ' ', '>', '/', '\t' };
+
+            int pos = tag.IndexOfAny(endchars, 1);
+            if (pos > 0) tag = tag.Substring(0, pos);
+            if (tag[0] == '/') tag = tag.Substring(1);
+
+            foreach (string aTag in allowedTags)
+            {
+                if (tag == aTag) return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
