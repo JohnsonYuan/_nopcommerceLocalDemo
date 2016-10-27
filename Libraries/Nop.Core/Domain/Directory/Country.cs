daa47b5714867d6295827aa3b7a614dd28d55208
JohnsonJohnsonYuan <johnson@lixin.com>
2016-10-27 23:13:16 +0800

    Add Domain/Directory

---
+++ Libraries/Nop.Core/Domain/Directory/Country.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Stores;
+
+namespace Nop.Core.Domain.Directory
+{
+    public class Country : BaseEntity, ILocalizedEntity, IStoreMappingSupported,
+    {
+        private ICollection<StateProvince> _stateProvinces;
+        private ICollection<ShippingMethod> _restrictedShippingMethods;
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether billing is allowed to this country
+        /// </summary>
+        public bool AllowsBilling { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether shipping is allowed to this country
+        /// </summary>
+        public bool AllowsShipping { get; set; }
+
+        /// <summary>
+        /// Gets or sets the two letter ISO code
+        /// </summary>
+        public string TwoLetterIsoCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the three letter ISO code
+        /// </summary>
+        public string ThreeLetterIsoCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the numeric ISO code
+        /// </summary>
+        public int NumericIsoCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether customers in this country must be charged EU VAT
+        /// </summary>
+        public bool SubjectToVat { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is limited/restricted to certain stores
+        /// </summary>
+        public bool LimitedToStores { get; set; }
+
+        public virtual ICollection<StateProvince> StateProvinces
+        {
+            get { return _stateProvinces ?? (_stateProvinces = new List<StateProvince>()); }
+            set { _stateProvinces = value; }
+        }
+
+        public virtual ICollection<ShippingMethod> RestrictedShippingMethods
+        {
+            get { return _restrictedShippingMethods ?? (_restrictedShippingMethods = new List<ShippingMethod>()); }
+            set { _restrictedShippingMethods = value; }
+        }
+
+    }
+}
---
+++ Libraries/Nop.Core/Domain/Directory/MeasureSettings.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Core.Domain.Directory
+{
+    public class MeasureSettings : ISettings
+    {
+        public int BaseDimensionId { get; set; }
+        public int BaseWeightId { get; set; }
+    }
+}
