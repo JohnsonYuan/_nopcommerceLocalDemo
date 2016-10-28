fc965f5b41c66552e6f7b631f5fa3ffe90f71c09
Jhonson@Rising <ylxin1993@hotmail.com>
2016-10-28 16:21:25 +0800

    Add Domain\Discount

---
+++ Libraries/Nop.Core/Domain/Discounts/DiscountUsageHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Nop.Core.Domain.Discounts
+{
+    /// <summary>
+    /// Represents a discount usage history entry
+    /// </summary>
+    public partial class DiscountUsageHistory : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the discount identifier
+        /// </summary>
+        public int DiscountId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order identifier
+        /// </summary>
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the discount
+        /// </summary>
+        public virtual Discount Discount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order
+        /// </summary>
+        public virtual Order Order { get; set; }
+    }
+}
