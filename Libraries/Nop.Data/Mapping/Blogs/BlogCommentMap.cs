61cb264bd66f1fe6a9ff73e3915f0852bf5ec9c3
JohnsonJohnsonYuan <johnson@lixin.com>
2016-10-31 22:09:15 +0800

    add Nop.Data mapping - AffiliateMap and Blogs

---
+++ Libraries/Nop.Data/Mapping/Blogs/BlogCommentMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nop.Core.Domain.Blogs;
+
+namespace Nop.Data.Mapping.Blogs
+{
+    public partial class BlogCommentMap : NopEntityTypeConfiguration<BlogComment>
+    {
+        public BlogCommentMap()
+        {
+            this.ToTable("BlogComment");
+            this.HasKey(pr => pr.Id);
+
+            this.HasRequired(bc => bc.BlogPost)
+                .WithMany(bp => bp.BlogComments)
+                .HasForeignKey(bc => bc.BlogPostId);
+
+            this.HasRequired(bc => bc.Customer)
+                .WithMany()
+                .HasForeignKey(bc => bc.CustomerId);
+        }
+    }
+}
