e4842356eb95e0b0a8fb6b58cbb57df0130faf50
Jhonson@Rising <ylxin1993@hotmail.com>
2016-11-17 09:36:56 +0800

    Update Nop.Web.Framework.csproj after merge from home owrk

--- Presentation/Nop.Web.Framework/WebWorkContext.cs
+++ Presentation/Nop.Web.Framework/WebWorkContext.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using Nop.Core;
 using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Directory;
 using Nop.Core.Domain.Localization;
 using Nop.Core.Domain.Tax;
-using System.Web;
 using Nop.Services.Customers;
 using Nop.Services.Vendors;
 using Nop.Services.Authentication;
