63898fb2c91a10c522dc55a6a7d81b50caa90a29
Jhonson@Rising <ylxin1993@hotmail.com>
2016-11-23 17:11:57 +0800

    Nop.Web.Framework Security

--- Presentation/Nop.WebMVC.Demo/WebForm1.aspx.cs
+++ Presentation/Nop.WebMVC.Demo/WebForm1.aspx.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
 
 namespace Nop.WebMVC.Demo.Views
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
+        { 
         }
     }
 }
