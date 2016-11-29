b53343241e65388ffae82cd929f88c0d143aae8a
JohnsonJohnsonYuan <johnson@lixin.com>
2016-11-29 23:50:01 +0800

    Nop.Web.Framework/DependencyRegistrar.cs

--- Presentation/Nop.Web.Framework/DependencyRegistrar.cs
+++ Presentation/Nop.Web.Framework/DependencyRegistrar.cs
@@ -5,11 +5,17 @@ using System.Reflection;
 using Autofac;
 using Autofac.Builder;
 using Autofac.Core;
+using Autofac.Integration.Mvc;
 using Nop.Core;
 using Nop.Core.Configuration;
+using Nop.Core.Data;
 using Nop.Core.Infrastructure;
 using Nop.Core.Infrastructure.DependencyManagement;
 using Nop.Services.Configuration;
+using System.Web;
+using Nop.Core.Fakes;
+using Nop.Services.Helpers;
+using Nop.Data;
 
 namespace Nop.Web.Framework
 {
@@ -21,9 +27,57 @@ namespace Nop.Web.Framework
         /// <param name="builder">Container builder</param>
         /// <param name="typeFinder">Type finder</param>
         /// <param name="config">Config</param>
-        public void Register(global::Autofac.ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
+        public void Register(ContainerBuilder builder, ITypeFinder typeFinder, NopConfig config)
         {
-            throw new NotImplementedException();
+            //HTTP context and other related stuff
+            builder.Register(c =>
+                //register FakeHttpContext when HttpContext is not available
+                HttpContext.Current != null ?
+                (new HttpContextWrapper(HttpContext.Current) as HttpContextBase) :
+                (new FakeHttpContext("~/") as HttpContextBase))
+                .As<HttpContextBase>()
+                .InstancePerLifetimeScope();
+            builder.Register(c => c.Resolve<HttpContextBase>().Request)
+                .As<HttpRequestBase>()
+                .InstancePerLifetimeScope();
+            builder.Register(c => c.Resolve<HttpContextBase>().Response)
+                .As<HttpResponseBase>()
+                .InstancePerLifetimeScope();
+            builder.Register(c => c.Resolve<HttpContextBase>().Session)
+                .As<HttpSessionStateBase>()
+                .InstancePerLifetimeScope();
+
+            //web helper
+            builder.RegisterType<WebHelper>().As<IWebHelper>().InstancePerLifetimeScope();
+            //user agent helper
+            builder.RegisterType<UserAgentHelper>().As<IUserAgentHelper>().InstancePerLifetimeScope();
+
+
+            //controllers
+            builder.RegisterControllers(typeFinder.GetAssemblies().ToArray());
+
+            //data layer
+            var dataSettingsManager = new DataSettingsManager();
+            var dataProviderSettings = dataSettingsManager.LoadSettings();
+            builder.Register(c => dataSettingsManager.LoadSettings()).As<DataSettings>();
+            builder.Register(x => new EfDataProviderManager(x.Resolve<DataSettings>())).As<BaseDataProviderManager>().InstancePerDependency();
+
+            builder.Register(x => x.Resolve<BaseDataProviderManager>().LoadDataProvider()).As<IDataProvider>().InstancePerDependency();
+
+            if (dataProviderSettings != null && dataProviderSettings.IsValid())
+            {
+                var efDataProviderManager = new EfDataProviderManager(dataSettingsManager.LoadSettings());
+                var dataProvider = efDataProviderManager.LoadDataProvider();
+                dataProvider.InitConnectionFactory();
+
+                builder.Register<IDbContext>(c => new NopObjectContext(dataProviderSettings.DataConnectionString)).InstancePerLifetimeScope();
+            }
+            else
+            {
+                builder.Register<IDbContext>(c => new NopObjectContext(dataSettingsManager.LoadSettings().DataConnectionString)).InstancePerLifetimeScope();
+            }
+
+            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
         }
 
         /// <summary>
@@ -38,15 +92,18 @@ namespace Nop.Web.Framework
     public class SettingsSource : IRegistrationSource
     {
         static readonly MethodInfo BuildMethod = typeof(SettingsSource).GetMethod(
-            "BuildRegistration", BindingFlags.Static | BindingFlags.NonPublic);
+            "BuildRegistration",
+            BindingFlags.Static | BindingFlags.NonPublic
+            );
 
         public IEnumerable<IComponentRegistration> RegistrationsFor(Service service, Func<Service, IEnumerable<IComponentRegistration>> registrationAccessor)
         {
             var ts = service as TypedService;
             if (ts != null && typeof(ISettings).IsAssignableFrom(ts.ServiceType))
             {
+                //这里才知道具体的要Resolve Settings
                 var buildMethod = BuildMethod.MakeGenericMethod(ts.ServiceType);
-                yield return (IComponentRegistration)BuildMethod.Invoke(null, null);
+                yield return (IComponentRegistration)buildMethod.Invoke(null, null);
             }
         }
 
@@ -70,5 +127,4 @@ namespace Nop.Web.Framework
 
         public bool IsAdapterForIndividualComponents { get { return false; } }
     }
-
 }
