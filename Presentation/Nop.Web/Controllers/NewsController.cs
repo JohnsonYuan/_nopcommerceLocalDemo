fd4fbc3a3b9ef7242d5c60da46be376ca29236e7
Jhonson@Rising <ylxin1993@hotmail.com>
2016-12-23 15:29:48 +0800

    News and polls in homepage

--- Presentation/Nop.Web/Controllers/NewsController.cs
+++ Presentation/Nop.Web/Controllers/NewsController.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Nop.Core;
 using Nop.Core.Domain.News;
 using Nop.Services.Localization;
 using Nop.Web.Framework.Security;
+using Nop.Web.Models.News;
+using Nop.Services.Seo;
+using Nop.Services.News;
+using Nop.Services.Media;
+using Nop.Services.Helpers;
+using Nop.Services.Messages;
+using Nop.Core.Caching;
+using Nop.Services.Logging;
+using Nop.Services.Stores;
+using Nop.Services.Security;
+using Nop.Core.Domain.Media;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Customers;
+using Nop.Web.Framework.Security.Captcha;
+using Nop.Services.Customers;
+using Nop.Services.Common;
+using Nop.Web.Infrastructure.Cache;
 
 namespace Nop.Web.Controllers
 {
@@ -14,24 +30,63 @@ namespace Nop.Web.Controllers
     {
         #region Fields
 
-        private readonly IWebHelper _webHelper;
+        private readonly INewsService _newsService;
         private readonly IWorkContext _workContext;
         private readonly IStoreContext _storeContext;
+        private readonly IPictureService _pictureService;
+        private readonly ILocalizationService _localizationService;
+        private readonly IDateTimeHelper _dateTimeHelper;
+        private readonly IWorkflowMessageService _workflowMessageService;
+        private readonly IWebHelper _webHelper;
+        private readonly ICacheManager _cacheManager;
+        private readonly ICustomerActivityService _customerActivityService;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly IPermissionService _permissionService;
+
+        private readonly MediaSettings _mediaSettings;
         private readonly NewsSettings _newsSettings;
+        private readonly LocalizationSettings _localizationSettings;
+        private readonly CustomerSettings _customerSettings;
+        private readonly CaptchaSettings _captchaSettings;
+
 
         #endregion
 
         #region Constructors
 
-        public NewsController(IWebHelper webHelper,
+        public NewsController(INewsService newsService,
             IWorkContext workContext,
             IStoreContext storeContext,
-            NewsSettings newsSettings)
+            IPictureService pictureService,
+            ILocalizationService localizationService,
+            IDateTimeHelper dateTimeHelper,
+            IWorkflowMessageService workflowMessageService,
+            IWebHelper webHelper,
+            ICacheManager cacheManager,
+            ICustomerActivityService customerActivityService,
+            IStoreMappingService storeMappingService,
+            IPermissionService permissionService,
+            MediaSettings mediaSettings,
+            NewsSettings newsSettings,
+            LocalizationSettings localizationSettings,
+            CustomerSettings customerSettings,
+            CaptchaSettings captchaSettings)
         {
-            this._webHelper = webHelper;
+            this._newsService = newsService;
             this._workContext = workContext;
             this._storeContext = storeContext;
+            this._pictureService = pictureService;
+            this._localizationService = localizationService;
+            this._dateTimeHelper = dateTimeHelper;
+            this._workflowMessageService = workflowMessageService;
+            this._webHelper = webHelper;
+            this._cacheManager = cacheManager;
+
+            this._mediaSettings = mediaSettings;
             this._newsSettings = newsSettings;
+            this._localizationSettings = localizationSettings;
+            this._customerSettings = customerSettings;
+            this._captchaSettings = captchaSettings;
         }
 
         #endregion
@@ -41,12 +96,87 @@ namespace Nop.Web.Controllers
         [NonAction]
         protected virtual void PrepareNewsItemModel(NewsItemModel model, NewsItem newsItem, bool prepareComments)
         {
+            if (newsItem == null)
+                throw new ArgumentNullException("newsItem");
 
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            model.Id = newsItem.Id;
+            model.MetaTitle = newsItem.MetaTitle;
+            model.MetaDescription = newsItem.MetaDescription;
+            model.MetaKeywords = newsItem.MetaKeywords;
+            model.SeName = newsItem.GetSeName(newsItem.LanguageId, ensureTwoPublishedLanguages: false);
+            model.Title = newsItem.Title;
+            model.Short = newsItem.Short;
+            model.Full = newsItem.Full;
+            model.AllowComments = newsItem.AllowComments;
+            model.CreatedOn = _dateTimeHelper.ConvertToUserTime(newsItem.StartDateUtc ?? newsItem.CreatedOnUtc, DateTimeKind.Utc);
+            model.NumberOfComments = newsItem.CommentCount;
+            model.AddNewComment.DisplayCaptcha = _captchaSettings.Enabled && _captchaSettings.ShowOnNewsCommentPage;
+            if (prepareComments)
+            {
+                var newsComments = newsItem.NewsComments.OrderBy(pr => pr.CreatedOnUtc);
+                foreach (var nc in newsComments)
+                {
+                    var commentModel = new NewsCommentModel
+                    {
+                        Id = nc.Id,
+                        CustomerId = nc.CustomerId,
+                        CustomerName = nc.Customer.FormatUserName(),
+                        CommentTitle = nc.CommentTitle,
+                        CommentText = nc.CommentText,
+                        CreatedOn = _dateTimeHelper.ConvertToUserTime(nc.CreatedOnUtc, DateTimeKind.Utc),
+                        AllowViewingProfiles = _customerSettings.AllowViewingProfiles && nc.Customer != null && !nc.Customer.IsGuest(),
+                    };
+                    if (_customerSettings.AllowCustomersToUploadAvatars)
+                    {
+                        commentModel.CustomerAvatarUrl = _pictureService.GetPictureUrl(
+                            nc.Customer.GetAttribute<int>(SystemCustomerAttributeNames.AvatarPictureId),
+                            _mediaSettings.AvatarPictureSize,
+                            _customerSettings.DefaultAvatarEnabled,
+                            defaultPictureType: PictureType.Avatar);
+                    }
+                    model.Comments.Add(commentModel);
+                }
+            }
         }
 
         #endregion
 
         #region Methods
+
+        public ActionResult HomePageNews()
+        {
+            if (!_newsSettings.Enabled || !_newsSettings.ShowNewsOnMainPage)
+                return Content("");
+
+            var cacheKey = string.Format(ModelCacheEventConsumer.HOMEPAGE_NEWSMODEL_KEY, _workContext.WorkingLanguage.Id, _storeContext.CurrentStore.Id);
+            var cachedModel = _cacheManager.Get(cacheKey, () =>
+            {
+                var newsItems = _newsService.GetAllNews(_workContext.WorkingLanguage.Id, _storeContext.CurrentStore.Id, 0, _newsSettings.MainPageNewsCount);
+                return new HomePageNewsItemsModel
+                {
+                    WorkingLanguageId = _workContext.WorkingLanguage.Id,
+                    NewsItems = newsItems
+                        .Select(x =>
+                                    {
+                                        var newsModel = new NewsItemModel();
+                                        PrepareNewsItemModel(newsModel, x, false);
+                                        return newsModel;
+                                    })
+                        .ToList()
+                };
+            });
+
+            //"Comments" property of "NewsItemModel" object depends on the current customer.
+            //Furthermore, we just don't need it for home page news. So let's reset it.
+            //But first we need to clone the cached model (the updated one should not be cached)
+            var model = (HomePageNewsItemsModel)cachedModel.Clone();
+            foreach (var newsItemModel in model.NewsItems)
+                newsItemModel.Comments.Clear();
+            return PartialView(model);
+        }
 
         [ChildActionOnly]
         public ActionResult RssHeaderLink()
--- Presentation/Nop.Web/Models/News/NewsCommentModel.cs
+++ Presentation/Nop.Web/Models/News/NewsCommentModel.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Web.Framework.Mvc;
+﻿using System;
+using Nop.Web.Framework.Mvc;
 
 namespace Nop.Web.Models.News
 {
