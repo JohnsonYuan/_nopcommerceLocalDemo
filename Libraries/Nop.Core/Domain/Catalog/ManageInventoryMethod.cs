8a5db8053e6f4201f919575816f7303794ce1d97
JohnsonJohnsonYuan <johnson@lixin.com>
2017-02-28 00:37:49 +0800

    update doc

--- Libraries/Nop.Core/Domain/Catalog/ManageInventoryMethod.cs
+++ Libraries/Nop.Core/Domain/Catalog/ManageInventoryMethod.cs
@@ -2,6 +2,10 @@ namespace Nop.Core.Domain.Catalog
 {
     /// <summary>
     /// Represents a method of inventory management
+    /// 我的注释: 在 LocaleStringResource 表中保存的值为 (调用Nop.Services.Extensions ToSelectList会使用localized值, key为Enums.Nop.Core.Domain.Catalog.ManageInventoryMethod.DontManageStock)
+    /// Don't track inventory
+    /// Track inventory
+    /// Track inventory by product attributes
     /// </summary>
     public enum ManageInventoryMethod
     {
